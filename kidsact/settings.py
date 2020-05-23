05ca86361935b14acdeb6c2b7a600c6c720ce6ef
cmetic <48930722+kkorang4@users.noreply.github.com>
2020-05-23 23:24:20 +1200

    changed settings

--- kidsact/settings.py
+++ kidsact/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'page/static')
+STATIC_URL = '/page/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
